75ca3c76b2414be818705f9dd740d4d93b11ffa9
Apprentice <apprentice@dbc29.local>
2013-02-08 14:50:27 -0800

    Create rough skeleton of methods

---
+++ list.rb
@@ -0,0 +1,39 @@
+class List
+  FILENAME = "task.txt"
+  attr_reader :tasks
+
+  def initialize
+    
+  end
+
+  def add(task)
+    
+  end
+
+  def delete!(id)
+    
+  end
+
+  def complete(id)
+    
+  end
+
+  def count
+    
+  end
+
+  def save
+    
+  end
+
+  def list_all
+    
+  end
+
+
+  private
+
+  def parse_list
+    
+  end
+end 
