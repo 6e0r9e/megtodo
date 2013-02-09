6c948c114c4438462f3d28622167b3f94ff18248
Apprentice <apprentice@dbc29.local>
2013-02-08 18:55:47 -0800

    Implement logic of display list

--- todocontroller.rb
+++ todocontroller.rb
@@ -2,12 +2,37 @@ require_relative 'list'
 require_relative 'todoview'
 
 class TODOController
- def initialize
-  @list = List.new
-  @view = ToDoView.new
- end
+  NUMBER = /\d+/
+  def initialize
+    @list = List.new
+    @view = ToDoView.new
+  end
 
- def method_name
- 	
+ def start
+  command = ARGV.shift
+
+  case command
+  when "add"
+    @list.add(ARGV.join(' '))
+    @view.confirm_add
+  when "list"
+    @view.display_list_header
+    @list.tasks.each do |task|
+      @view.display_task(task.id, task.completed, task.text)
+    end
+  when "delete"
+    id = ARGV.shift
+    @list.delete!(id)
+    @view.confirm_delete
+  when "help"
+    @view.help
+  when "complete"
+    @list.complete(ARGV.shift)
+  else
+    @view.error
+  end
+
  end
 end
+
+TODOController.new.start
